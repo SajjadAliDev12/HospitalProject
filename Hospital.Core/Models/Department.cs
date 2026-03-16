0b2e7260910c5373f4641e21b814ba4ff063b6d9
sajjd12 <100775607+sajjd12@users.noreply.github.com>
2026-03-16 04:22:44 +0300

    initial commit

---
+++ Hospital.Core/Models/Department.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Core.Models
+{
+    public class Department
+    {
+        [Required]
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        public bool isDeleted { get; set; } = false;
+        public virtual ICollection<Employee> Employees { get; set; } = new List<Employee>();
+    }
+}
---
+++ Hospital.Core/Models/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Core.Models
+{
+    public class Employee
+    {
+        [Required]
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        [MaxLength(250)]
+        public string Name { get; set; }
+        public DateOnly BirthDate { get; set; }
+        public DateOnly HireDate { get; set; }
+        public int DepartmentId { get; set; }
+        public int ShiftType { get; set; }
+        public int JobTitle { get; set; }
+        public int Gender { get; set; }
+        public int CertificateType { get; set; }
+        public int LeaveBalance { get; set; }
+        [MaxLength(500)]
+        public string Address { get; set; }
+        [MaxLength(20)]
+        public string PhoneNumber { get; set; }
+        public int JobStatus { get; set; }
+        public bool isDeleted { get; set; } = false;
+        public virtual Department Department { get; set; }
+    }
+}
